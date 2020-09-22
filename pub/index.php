70cbee2c2538895de1103f39b76ca1a296e320fe
jsostaric <bugificiran@gmail.com>
2020-09-22 12:43:43 +0200

    Revert "router setup"
    This reverts commit 25fd8c8331e6cde72396d085262ffdab873ea25a.

--- pub/index.php
+++ pub/index.php
@@ -1,17 +1,3 @@
 <?php
 
-define('BP',  dirname(__DIR__));
-
-spl_autoload_register(function ($class){
-    $class = lcfirst($class);
-    $className = BP . DIRECTORY_SEPARATOR . strtr($class, '\\' , DIRECTORY_SEPARATOR) . '.php';
-
-   if(file_exists($className)){
-       return require_once $className;
-   }
-});
-
-session_start();
-
-App\Core\Router::load('../app/routes.php')
-    ->direct(App\Core\Request::uri(), App\Core\Request::method());
+echo 'Jurica';
